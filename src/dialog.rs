3b46ec2f7c106c1bc9d536de63a6a7bc44eee42d
uselessgoddess <dodickgod@gmail.com>
2025-04-18 13:13:38 +0300

    add `rfd` as file dialog

--- src/dialog.rs
+++ src/dialog.rs
@@ -1,11 +1,68 @@
-use bevy::prelude::*;
+use {
+  bevy::{prelude::*, tasks::AsyncComputeTaskPool},
+  crossbeam_channel::{Receiver, Sender, bounded},
+  rfd::AsyncFileDialog,
+};
 
-use crossbeam_channel::{Receiver, Sender, bounded};
-
-#[derive(Resource)]
+#[derive(Resource, Deref)]
 struct Tx<T>(Sender<T>);
 
-#[derive(Resource)]
+#[derive(Resource, Deref)]
 struct Rx<T>(Receiver<T>);
 
-pub fn plugin(app: &mut App) {}
+#[derive(Event)]
+pub struct Open {
+  pub content: Vec<u8>,
+}
+
+pub fn plugin(app: &mut App) {
+  let (tx, rx) = bounded(32);
+
+  app
+    .add_event::<Open>()
+    .insert_resource(Tx::<Open>(tx))
+    .insert_resource(Rx::<Open>(rx))
+    .add_systems(Update, receive_picks::<Open>);
+}
+
+fn receive_picks<E: Event>(rx: Res<Rx<E>>, mut events: EventWriter<E>) {
+  for open in rx.try_iter() {
+    events.send(open);
+  }
+}
+
+// #[derive(Deref, DerefMut)]
+pub struct FileDialog<'w, 's, 'a> {
+  commands: &'a mut Commands<'w, 's>,
+  // #[deref]
+  dialog: AsyncFileDialog,
+}
+
+impl<'w, 's, 'a> FileDialog<'w, 's, 'a> {
+  pub fn pick_file(self) {
+    self.commands.queue(|world: &mut World| {
+      let tx = world.resource::<Tx<Open>>().0.clone();
+
+      AsyncComputeTaskPool::get()
+        .spawn(async move {
+          let Some(file) = self.dialog.pick_file().await else {
+            return;
+          };
+
+          tx.send(Open { content: file.read().await }).unwrap();
+        })
+        .detach();
+    });
+  }
+}
+
+pub trait FileDialogExt<'w, 's> {
+  #[must_use]
+  fn dialog<'a>(&'a mut self) -> FileDialog<'w, 's, 'a>;
+}
+
+impl<'w, 's> FileDialogExt<'w, 's> for Commands<'w, 's> {
+  fn dialog<'a>(&'a mut self) -> FileDialog<'w, 's, 'a> {
+    FileDialog { commands: self, dialog: AsyncFileDialog::new() }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,38 @@ use bevy::{
   asset::RenderAssetUsages,
   prelude::*,
   render::render_resource::{Extent3d, TextureDimension, TextureFormat},
-  tasks::AsyncComputeTaskPool,
 };
 
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
+    .add_plugins(dialog::plugin)
     .add_systems(Startup, setup)
-    .add_systems(Update, pick)
+    .add_systems(Update, (pick, update))
     .run();
 }
 
-mod image {
+mod dim {
   pub const W: u32 = 256;
   pub const H: u32 = 256;
 }
 
-#[derive(Resource)]
+fn render_usage() -> RenderAssetUsages {
+  RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD
+}
+
+#[derive(Resource, Deref)]
 struct Process(Handle<Image>);
 
 fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
-  let mut image = Image::new_fill(
-    Extent3d { width: image::W, height: image::H, depth_or_array_layers: 1 },
+  commands.spawn(Camera2d);
+
+  let image = Image::new_fill(
+    Extent3d { width: dim::W, height: dim::H, depth_or_array_layers: 1 },
     TextureDimension::D2,
-    &Srgba::BLACK.to_u8_array(),
+    &Srgba::WHITE.to_u8_array(),
     TextureFormat::Rgba8UnormSrgb,
-    RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    render_usage(),
   );
 
   let handle = images.add(image);
@@ -38,14 +44,29 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
   commands.spawn(Sprite::from_image(handle));
 }
 
-use rfd::AsyncFileDialog;
+use {
+  dialog::{FileDialogExt, Open},
+  image::imageops::FilterType,
+};
 
-fn pick(input: Res<ButtonInput<KeyCode>>) {
-  let task = async move {
-    AsyncFileDialog::new().pick_file().await;
-  };
-
+fn pick(input: Res<ButtonInput<KeyCode>>, mut commands: Commands) {
   if input.just_pressed(KeyCode::Space) {
-    AsyncComputeTaskPool::get().spawn(task).detach();
+    commands.dialog().pick_file();
+  }
+}
+
+fn update(
+  mut events: EventReader<Open>,
+  mut images: ResMut<Assets<Image>>,
+  process: Res<Process>,
+) {
+  for Open { content } in events.read() {
+    if let Ok(dynamic) = image::load_from_memory(&content) {
+      let Some(image) = images.get_mut(process.id()) else { continue };
+
+      let dynamic = dynamic.resize_exact(dim::W, dim::H, FilterType::Nearest);
+
+      *image = Image::from_dynamic(dynamic, true, render_usage());
+    }
   }
 }
